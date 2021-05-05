f6c9c4e7da419496ba6d07c005cde48b104a2c2a
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-05-05 22:51:45 +0530

    Added: code of append method

--- src/com/LinkedList/MyLinkedList.java
+++ src/com/LinkedList/MyLinkedList.java
@@ -3,12 +3,19 @@ package com.LinkedList;
 public class MyLinkedList {
 
 	public static void main(String[] args) {
-		LinkedList linkedList=new LinkedList();
-		linkedList.push(70);
-	    linkedList.push(30);
-		linkedList.push(56);
+		LinkedList linkedList1=new LinkedList();
+		linkedList1.push(70);
+	    linkedList1.push(30);
+		linkedList1.push(56);
+		linkedList1.printList();
 		
-		linkedList.printList();
+		System.out.println("\n");
+		
+		LinkedList linkedList2 = new LinkedList();
+		linkedList2.append(70);
+	    linkedList2.append(30);
+		linkedList2.append(56);
+		linkedList2.printList();
 
 	}
 
@@ -32,6 +39,20 @@ class LinkedList{
 	 	newNode.next=head;
 	 	head=newNode;	
 	     }
+	
+	 public void append(int data) {
+	    Node newNode = new Node(data);
+	    if(head == null) {
+		head= newNode;
+		return;
+	    }
+	 
+	    Node last= head;
+	    while(last.next != null) {
+		last = last.next;
+	    }
+	    last.next=newNode;
+ }
 	
 	public void printList() {
 		System.out.print("LinkedList:");
