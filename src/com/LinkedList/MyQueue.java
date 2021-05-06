81ab4f69847d5f1b5895cfec7fbc4acdb3758d38
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-05-06 18:51:06 +0530

    Create Queue from linkedList

---
+++ src/com/LinkedList/MyQueue.java
@@ -0,0 +1,15 @@
+package com.LinkedList;
+
+public class MyQueue {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Queue :\n");
+		LinkedList linkedList1 = new LinkedList();
+		linkedList1.push(70);
+		linkedList1.push(30);
+		linkedList1.push(56);
+		linkedList1.printList();
+	}
+
+}
