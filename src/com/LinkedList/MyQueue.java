4c71202e0befcd94cc3090e95f23e0e9fac819fe
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-05-07 19:47:06 +0530

    Queue enqueue method using array

--- src/com/LinkedList/MyQueue.java
+++ src/com/LinkedList/MyQueue.java
@@ -4,16 +4,55 @@ public class MyQueue {
 
 	public static void main(String[] args) {
 		
-		System.out.print("Queue :\n");
-		LinkedList linkedList1 = new LinkedList();
-		linkedList1.append(56);
-		linkedList1.append(30);
-		linkedList1.append(70);
-		linkedList1.printList();
+		Queue queue = new Queue(10);
+		queue.enqueue(10);
+		queue.enqueue(20);
+		queue.enqueue(30);
 		
-		System.out.print("\nQueue after Pop:\n");
-		linkedList1.pop();
-		linkedList1.printList();
+		queue.print();
 		
 	}
 }
+
+class Queue {
+	
+	int front, rear, size;
+	int que [];
+	int capacity;
+	
+	public Queue(int capacity) {
+		this.capacity= capacity;
+		this.que=new int[capacity];
+		this.size = -1;
+		this.front= 0;
+		this.rear = -1;
+	}
+	
+	public void enqueue (int data) {
+		
+		if(isFull()) {
+		   System.out.println("Queue is full");
+		   return;			
+		}else {
+		rear++;
+		if(rear == capacity-1)
+		   rear=0;
+		}
+		que[rear]=data;
+		size++;
+	}
+
+	public boolean isFull() {
+		return size == capacity;
+	}
+	
+	public void print() {
+		System.out.println("Queue element are:");
+		
+		for(int ele=0; ele <=this.size; ele++) {
+		   System.out.print(que[ele]+" ");
+		}
+		
+		System.out.println();
+	}
+}	
